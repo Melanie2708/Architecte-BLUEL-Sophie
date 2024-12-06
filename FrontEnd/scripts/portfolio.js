2e0556104002415a788f2db549b066924ff81792
Mélanie KRAEMER <m_kraemer@orange.fr>
2024-12-06 17:02:04 +0100

    Etape 1.1:Récupération des travaux

---
+++ FrontEnd/scripts/portfolio.js
@@ -0,0 +1,25 @@
+async function recupererProjets (){
+    const reponse = await fetch("http://localhost:5678/api/works");
+    const projets = await reponse.json();
+return projets;
+}
+
+
+function afficherProjets(projets){
+    for (let i = 0; i < projets.length; i++) {
+        const projet = projets [i];
+        // Récupération de l'élément
+        const divGallery=document.querySelector(".gallery");
+        //Création de la figure
+        const figure = document.createElement ("figure");
+        const imageFigure = document.createElement("img");
+        imageFigure.src = projet.imageUrl;
+        imageFigure.alt = projet.title;
+        const caption = document.createElement("figcaption");
+        caption.innerText= projet.title;
+
+        divGallery.appendChild(figure);
+		figure.appendChild(imageFigure);
+		figure.appendChild(caption);
+}}
+recupererProjets().then(projets =>{afficherProjets(projets);});
