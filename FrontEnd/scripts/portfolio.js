9cad2571984f2dc7ccf1b9e929d5abb14c3c6a26
Mélanie KRAEMER <m_kraemer@orange.fr>
2024-12-09 21:46:29 +0100

    Ajout filtres

--- FrontEnd/scripts/portfolio.js
+++ FrontEnd/scripts/portfolio.js
@@ -1,30 +1,83 @@
-async function recupererProjets (){
-    const reponse = await fetch("http://localhost:5678/api/works");
-    const projets = await reponse.json();
-return projets;
+//Fonction pour afficher les travaux
+
+async function recupererProjets() {
+  const reponse = await fetch("http://localhost:5678/api/works");
+  const projets = await reponse.json();
+  return projets;
 }
 
-//Fonction pour afficher les travaux
-function afficherProjets(projets){
-    for (let i = 0; i < projets.length; i++) {
-        const projet = projets [i];
-        // Récupération de l'élément
-        const divGallery=document.querySelector(".gallery");
-        //Création de la figure
-        const figure = document.createElement ("figure");
-        const imageFigure = document.createElement("img");
-        imageFigure.src = projet.imageUrl;
-        imageFigure.alt = projet.title;
-        const caption = document.createElement("figcaption");
-        caption.innerText= projet.title;
+function afficherProjets(projets, categorieId) {
+  // Récupération de l'élément
+  const divGallery = document.querySelector(".gallery");
+  divGallery.innerHTML = "";
 
-        divGallery.appendChild(figure);
-		figure.appendChild(imageFigure);
-		figure.appendChild(caption);
-}}
-recupererProjets().then(projets =>{afficherProjets(projets);});
+  for (let i = 0; i < projets.length; i++) {
+    const projet = projets[i];
+    if (projet.categoryId == categorieId || categorieId == 0) {
+      //Création de la figure
+      const figure = document.createElement("figure");
+      const imageFigure = document.createElement("img");
+      imageFigure.src = projet.imageUrl;
+      imageFigure.alt = projet.title;
+      const caption = document.createElement("figcaption");
+      caption.innerText = projet.title;
+
+      divGallery.appendChild(figure);
+      figure.appendChild(imageFigure);
+      figure.appendChild(caption);
+    }
+  }
+}
 
 //Fonction pour les filtres
 
-const btnObjets = document.querySelector(".btnObjets");
+async function recupererCategories() {
+  const reponse = await fetch("http://localhost:5678/api/categories");
+  const categories = await reponse.json();
+  const nouvelObjet = { id: 0, name: "Tous" };
+  categories.unshift(nouvelObjet);
+  return categories;
+}
 
+function clicFiltre(filtre) {
+  const listeFiltre = document.querySelectorAll(".filtre");
+  for (let i = 0; i < listeFiltre.length; i++) {
+    listeFiltre[i].classList.remove("filtre-actif");
+  }
+  filtre.classList.add("filtre-actif");
+  afficherProjets(tousProjets, filtre.id);
+}
+
+function afficherCategories(categories) {
+  for (let i = 0; i < categories.length; i++) {
+    const categorie = categories[i];
+    //Récupération de l'élément
+    const divFiltre = document.querySelector(".filtres");
+    //Création du bouton
+    const bouton = document.createElement("button");
+    bouton.textContent = categorie.name; // Ajoute le nom récupéré depuis l'API
+    bouton.id = categorie.id;
+    bouton.classList.add("filtre");
+    bouton.addEventListener("click", (event) => {
+      clicFiltre(event.target);
+    });
+
+    if (bouton.id == 0) {
+      bouton.classList.add("filtre-actif");
+    }
+
+    divFiltre.appendChild(bouton);
+  }
+}
+
+//Partie principale du script
+
+let tousProjets = {};
+
+recupererProjets().then((projets) => {
+  tousProjets = projets;
+  afficherProjets(tousProjets, 0);
+});
+recupererCategories().then((categories) => {
+  afficherCategories(categories);
+});
