aece4ddb0c4a1a5c6d6a19119a6385811fcd3fc7
Mélanie KRAEMER <m_kraemer@orange.fr>
2024-12-10 22:18:53 +0100

    Page login

---
+++ FrontEnd/scripts/login.js
@@ -0,0 +1,31 @@
+async function connexion() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("motdepasse").value;
+  const messageErreur = document.getElementById("message-erreur");
+
+  const reponse = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", accept: "application/json" },
+    body: JSON.stringify({ email: email, password: password }),
+  });
+
+  console.log(reponse);
+  if (reponse.status === 404) {
+    messageErreur.innerText = "Utilisateur inconnu";
+  } else if (reponse.status === 401) {
+    messageErreur.innerText = "Mot de passe erroné";
+  } else if (reponse.status === 200) {
+    const infoConnexion = await reponse.json();
+    localStorage.setItem("token", infoConnexion.token);
+    window.location.href = "index.html";
+  }
+}
+
+function ajoutListenerConnexion() {
+  const formulaire = document.getElementById("formulaire-login");
+  formulaire.addEventListener("submit", function (event) {
+    event.preventDefault();
+    connexion();
+  });
+}
+ajoutListenerConnexion();
